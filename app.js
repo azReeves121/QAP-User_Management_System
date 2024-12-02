76bcfc3d45846a5d1cd6b9e5ab460515eecfe584
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2024-12-02 01:26:22 -0330

    flash and session

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const initializePassport = require("./passport-config");
-
-initializePassport();
+const flash = require("express-flash");
+const session = require("express-session");
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 const user = [];
 
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.set("view engine", "ejs");
 
